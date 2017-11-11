988561a31ca135e3610eadecba267173ca99a475
JuPlutonic <8gzi0c+11ezb3n4oio0zf24zw@sharklasers.com>
2017-11-11 19:40:36 +0530

    1.Put some commented methods to application_controller to authorize objects (mb. need to be impelemented with 'cancancan' gem). 2.Fixed error 'Index method missing', put index/show methods to app/users_controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,22 @@
 # frozen_string_literal: true
 
+#Authorization, mb need refactoring using CanCanCan gem
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # helper_methods :have_rights_for?, :under_admin?
+
+  # def abort_if_non_authorized(object)
+  #   unless object.user == current_user
+  #   unless have_rights_for?(object)
+  #     redirect_to :root, alert: "У вас нет прав для выполнения этого действия"
+  #   end
+  # end
+
+  # def under_admin?
+  #   current_user && current_user.admin?
+  # end
+
+  # def have_rights_for?(object)
+  #   object && current_user && (current_user.author_of?(object) || current_user.admin?)
+  # end
 end
