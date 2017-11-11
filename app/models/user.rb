af697c0d730f6972687f1f44ad985c6e2c8d0af4
JuPlutonic <8gzi0c+11ezb3n4oio0zf24zw@sharklasers.com>
2017-11-11 13:05:34 +0530

    Devise. To User model added devise migration and route

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_one :profile
     #validates :email, uniqueness: true
 end
