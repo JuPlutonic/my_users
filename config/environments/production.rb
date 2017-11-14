2a0deb4acb965eb2f826122a406b5abe69607fde
JuPlutonic <8gzi0c+11ezb3n4oio0zf24zw@sharklasers.com>
2017-11-15 01:12:38 +0530

    Made Маilgun conf.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,15 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "my_users_#{Rails.env}"
   config.action_mailer.default_url_options = { host: 'warm-sea-63300.herokuapp.com' }
   config.action_mailer.perform_caching = false
+  config.action_mailer .delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+          :port                 => ENV['MAILGUN_SMTP_PORT'],
+          :address           => ENV['MAILGUN_SMTP_SERVER'],
+          :user_name       => ENV['MAILGUN_SMTP_LOGIN'],
+          :password         => ENV['MAILGUN_SMTP_PASSWORD'],
+          :domain            => 'warm-sea-63300.herokuapp.com',
+          :authentification => :plain,
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
