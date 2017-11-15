6032576ba58fb7909fb92d533d1f623509db40da
JuPlutonic <8gzi0c+11ezb3n4oio0zf24zw@sharklasers.com>
2017-11-16 05:02:23 +0530

    Fixed Create_user_spec

--- spec/acceptance/create_user_spec.rb
+++ spec/acceptance/create_user_spec.rb
@@ -9,13 +9,14 @@ feature 'Create user' do
 
   scenario 'User adds file when he registers' do
     fill_in t('.name'), with: "First user's name"
-    attach_file 'Avatar', "#{Rails.root}/spec/spec_helper.rb"
+    attach_file 'Avatar', "#{Rails.root}/public/favicon.png"
     fill_in t('.email'), with: 'mail@test.com'
     fill_in t('.password'), with: '123456'
     fill_in t('.password_confirmation'), with: '123456'
     click_on t('.submit')
 
     expect(page).to have_content t('.submit') # or text like 'request' or 'запрос'
+    expect(page).tp have_link 'favicon.png' # ------- need to be moved to an acceptance spec
   end
 
 #
